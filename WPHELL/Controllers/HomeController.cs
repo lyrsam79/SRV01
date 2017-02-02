b29c6f11f77d8b19874f6b6b55359d34a97d8b11
JEAN <LYRSAM79@GMAIL.COM>
2017-02-02 17:28:52 -0200

    FIRST COMMIT WEPHELL

---
+++ WPHELL/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WPHELL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+            //
+            if (true) { }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Nosso contato";
+
+            return View();
+        }
+    }
+}
---
+++ WPHELL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WPHELL.Startup))]
+namespace WPHELL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
